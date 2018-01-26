b768186c1f6083ef327e3771568ff29387733eb4
Bikram Bharti <bikram.bharti.ece15@itbhu.ac.in>
2018-01-27 01:47:49 +0530

    substitute url added

--- server.py
+++ server.py
@@ -93,7 +93,7 @@ api.add_resource(KeywordSearch,'/search')
 api.add_resource(Employees, '/employees') # Route_1
 api.add_resource(Tracks, '/tracks') # Route_2
 api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
-
+api.add_resource(Substitute, '/substitute') 
 
 if __name__ == '__main__':
      app.run()
